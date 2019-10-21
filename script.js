f276b5720c122a01f87052a60993ed9065e8050b
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-21 20:46:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let fullList = document.getElementById('tree');
+const fullList = document.getElementById('tree');
 fullList.addEventListener('click', event => {
   if (event.target.tagName !== 'LI') {
     return;
   }
-  let childrenList = event.target.children;
+  const childrenList = event.target.children;
 
   if (!childrenList[0].hidden) {
     childrenList[0].hidden = true;
