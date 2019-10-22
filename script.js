590adfa04c6085a645b40ce5ec6bbde45286cb98
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-22 21:30:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const fullList = document.getElementById('tree');
 fullList.addEventListener('click', event => {
-  if (event.target.tagName !== 'SPAN') {
+  const children = event.target.parentElement.querySelector('ul');
+  if (event.target.tagName !== 'SPAN' || !children) {
     return;
   }
-
-  const children = event.target.parentElement.querySelector('ul');
-
-  if (!children) return;
 
   children.hidden = !children.hidden;
 });
