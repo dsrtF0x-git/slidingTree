dc1443a8048be431f400d6568560a1a55dc2c00a
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-22 16:41:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const fullList = document.getElementById('tree');
-
 fullList.addEventListener('click', event => {
-  if (event.target.tagName !== 'LI') {
+  if (event.target.tagName !== 'SPAN') {
     return;
   }
-  const childrenList = event.target.children;
 
-  childrenList[0].hidden = !childrenList[0].hidden;
+  const children = event.target.parentElement.querySelector('ul');
+
+  if (!children) return;
+
+  children.hidden = !children.hidden;
 });
